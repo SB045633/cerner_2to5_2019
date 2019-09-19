225bf5bce49a9bda3a426c0cbcf40d26802ee3d5
SB045633 <55515003+SB045633@users.noreply.github.com>
2019-09-18 23:10:08 -0500

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -1,3 +1,5 @@
+# Displays the time remaining until CHC 2019, or a message if it has already started
+
 import datetime
 import time
 
@@ -7,7 +9,7 @@ chcTime = timeVar(year=2019,month=10,day=7,hour=7,minute=0,second=0)
 
 while timeNow < chcTime:
     timeNow = timeVar.now()
-    print("Time until CHC:", chcTime - timeVar(year=timeNow.year,month=timeNow.month,day=timeNow.day,hour=timeNow.hour,minute=timeNow.minute,second=timeNow.second), end='\r')
+    print('Time until CHC:', chcTime - timeVar(year=timeNow.year,month=timeNow.month,day=timeNow.day,hour=timeNow.hour,minute=timeNow.minute,second=timeNow.second), end='\r')
     time.sleep(1)
 
 if timeNow > chcTime:
