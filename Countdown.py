1f61b2fe33ac5d1d01c8cd0b8f994097ecf5d46a
SB045633 <55515003+SB045633@users.noreply.github.com>
2019-09-18 22:55:51 -0500

    Create Countdown.py

---
+++ Countdown.py
@@ -0,0 +1,14 @@
+import datetime
+import time
+
+timeVar = datetime.datetime
+timeNow = timeVar.now()
+chcTime = timeVar(year=2019,month=10,day=7,hour=7,minute=0,second=0)
+
+while timeNow < chcTime:
+    timeNow = timeVar.now()
+    print("Time until CHC:", chcTime - timeVar(year=timeNow.year,month=timeNow.month,day=timeNow.day,hour=timeNow.hour,minute=timeNow.minute,second=timeNow.second), end='\r')
+    time.sleep(1)
+
+if timeNow > chcTime:
+    print('CHC has begun!')
