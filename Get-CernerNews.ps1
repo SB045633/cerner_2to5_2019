da6173460f4857edb5678132b7e8afbe3a8b8fb7
Barclay, Shawn <Shawn.Barclay@cerner.com>
2019-09-21 17:47:42 -0500

    Update Get-CernerNews.ps1

--- Get-CernerNews.ps1
+++ Get-CernerNews.ps1
@@ -1,20 +1,15 @@
+# cerner_2^5_2019
+# Pulls Cerner related news from Google News and creates an HTML document with the results
+
 $news = Invoke-RestMethod -uri "https://news.google.com/rss/search?um=1&q=cerner&hl=en-US&gl=US&ceid=US:en"
-$news = $news | ForEach-Object {Add-Member -InputObject $_ -MemberType NoteProperty -Name "PubDateMod" -Value (Get-date $_.pubdate) -Passthru}
-$news = $news | Select-Object Title,PubDateMod,Link | Sort-Object PubDateMod -Descending | ConvertTo-Html -Fragment
+$news = $news | ForEach-Object {Add-Member -InputObject $_ -MemberType NoteProperty -Name "NewPubDate" -Value (Get-date $_.pubdate) -Passthru; Add-Member -InputObject $_ -MemberType NoteProperty -Name "HTMLLink" -Value "<a href='$($_.Link)'>Article Link</a>"}
+$news = ($news | Select-Object NewPubDate,Title,HTMLLink | Sort-Object PubDateMod -Descending | ConvertTo-Html -Fragment) -replace "title","Title" -replace "NewPubDate","Date" -replace "HTMLLink","Link"
 
 $header = @"
 <style>
-        table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-        }
-        td, th {
-        border: 1px solid #dddddd;
-        background-color: #dddddd
-        text-align: left;
-        padding: 8px;
-        }
+        table {font-family: arial, sans-serif; width: 100%; border-collapse: collapse;}
+        td, th {border: 2px solid #dddddd; text-align: left; padding: 8px;}
+        tr:nth-child(even){background-color: #d1d1d1;}
 </style>
 "@
 
@@ -22,10 +17,10 @@ $date = Get-Date -UFormat "%m%d%Y"
 $report = "$($env:TEMP)\CernerNewsFeed.html"
 $body = @"
 <title>Cerner News</title>
-<center><H1>Cerner News</H1></center>
+<center><font size="16" face="arial black">Cerner News</font></center><br>
 $($news)
 "@
 
-ConvertTo-Html -Head $header -Body $body | Set-Content $report
-
+$htmlOut = ConvertTo-Html -Head $header -Body $body
+[System.Web.HttpUtility]::HtmlDecode($htmlOut) | Set-Content $report
 Invoke-Item $report
